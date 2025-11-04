b47d3dc32aa17347244cd0dd7198aea19e5f84e9
codebyyanto <yanto.unix@gmail.com>
2025-11-05 03:33:04 +0700

    Menambahkan Inisialisasi Objek Utama di server.js (Freshora-API)

--- Freshora-API/server.js
+++ Freshora-API/server.js
@@ -5,3 +5,8 @@ const cors = require('cors');
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
+// inisialisasi Objek utama
+const prisma = new PrismaClient();
+const app = express();
+const PORT = process.env.PORT || 3000;
