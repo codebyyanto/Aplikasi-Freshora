ba26682534e34008006ab095fc73754c2fd8714b
codebyyanto <yanto.unix@gmail.com>
2025-11-05 03:30:57 +0700

    Update import library di server.js (Freshora-API)

---
+++ Freshora-API/server.js
@@ -0,0 +1,7 @@
+// import library
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
