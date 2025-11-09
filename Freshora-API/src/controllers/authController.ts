741453c21226109260c1f27732ea70f5affeef9e
codebyyanto <yanto.unix@gmail.com>
2025-11-10 02:10:38 +0700

    menambahkan function register di authController.ts (Freshora-API)

--- Freshora-API/src/controllers/authController.ts
+++ Freshora-API/src/controllers/authController.ts
@@ -3,3 +3,13 @@ import { prisma } from '../prismaClient';
 import bcrypt from 'bcrypt';
 import { signToken } from '../utils/jwt';
 
+export async function register(req: Request, res: Response) {
+  const { name, email, password, phone } = req.body;
+  if (!email || !password) return res.status(400).json({ message: 'email & password required' });
+  const exists = await prisma.user.findUnique({ where: { email }});
+  if (exists) return res.status(400).json({ message: 'Email sudah terdaftar' });
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await prisma.user.create({ data: { name, email, password: hashed, phone }});
+  const token = signToken({ id: user.id, email: user.email });
+  res.json({ user: { id: user.id, name: user.name, email: user.email }, token });
+}
