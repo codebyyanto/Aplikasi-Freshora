55e4ece080491222bf1e781f9ff922994bf1eaac
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-11-11 03:46:44 +0700

    Membuat file index.tsx di folder app dan menabahkan Import library dan komponen yang dibutuhkan (Freshora-mobile)

---
+++ Freshora-mobile/app/index.tsx
@@ -0,0 +1,14 @@
+// Import library dan komponen yang dibutuhkan
+import React from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import Swiper from "react-native-swiper"; // untuk membuat slide onboarding
+import { LinearGradient } from "expo-linear-gradient"; // efek gradasi tombol
+import { useRouter } from "expo-router"; // navigasi antar halaman
+
