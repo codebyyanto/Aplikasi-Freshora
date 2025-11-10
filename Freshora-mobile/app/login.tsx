19db290e3064d932bcfd3c6bc3ff96f1ef05cf8a
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-11-11 04:20:15 +0700

    Membuat file login.tsx di folder app dan menabahkan Import library dan komponen yang dibutuhkan (Freshora-mobile)

---
+++ Freshora-mobile/app/login.tsx
@@ -0,0 +1,18 @@
+// Import library dan komponen yang dibutuhkan
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Keyboard,
+  Platform,
+  Animated,
+  Easing,
+  Dimensions,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { useRouter } from "expo-router";
