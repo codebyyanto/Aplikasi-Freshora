78ef4f3e11cf84bb6e0c485a3037018f698d00bc
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-11-11 03:49:50 +0700

    Menambahkan fitur untuk ukuran layar perangkat di index.tsx di folder app (Freshora-mobile)

--- Freshora-mobile/app/index.tsx
+++ Freshora-mobile/app/index.tsx
@@ -12,3 +12,5 @@ import Swiper from "react-native-swiper"; // untuk membuat slide onboarding
 import { LinearGradient } from "expo-linear-gradient"; // efek gradasi tombol
 import { useRouter } from "expo-router"; // navigasi antar halaman
 
+// Ambil ukuran layar perangkat
+const { width, height } = Dimensions.get("window");
