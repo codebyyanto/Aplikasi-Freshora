acbb4d094a126427c2f7fee3acf7ac5f41948cee
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-11-16 01:27:57 +0700

    Menambahkan function RegisterScreen dan membaut  State untuk menyimpan data input user di register.tsx (Freshora-mobile)

--- Freshora-mobile/app/register.tsx
+++ Freshora-mobile/app/register.tsx
@@ -16,3 +16,12 @@ import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
 
 const { height } = Dimensions.get("window");
+
+export default function RegisterScreen() {
+  // State untuk menyimpan data input user
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
