3fe83ff4027837fd1e7f5ac2cd9571b4bbccfcb4
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-11-16 02:10:31 +0700

    Menambahkan style untuk komponen di app/home/index.tsx

--- Freshora-mobile/app/home/index.tsx
+++ Freshora-mobile/app/home/index.tsx
@@ -16,3 +16,16 @@ import { useRouter } from "expo-router";
 
 // Ambil ukuran layar perangkat
 const { width } = Dimensions.get("window");
+
+// style untuk komponen
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#fff", padding: 15 },
+  searchRow: { flexDirection: "row", alignItems: "center" },
+  searchBox: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#F3F3F3",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+  }
