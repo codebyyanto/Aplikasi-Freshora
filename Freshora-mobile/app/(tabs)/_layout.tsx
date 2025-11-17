862f01c05a84b412cad1566dc2a910b2f3399f57
codebyyanto <yanto.unix@gmail.com>
2025-11-17 16:12:10 +0700

    Menambahahkan function TabsLayout di file _layout.tsx di folder (tabs)

--- Freshora-mobile/app/(tabs)/_layout.tsx
+++ Freshora-mobile/app/(tabs)/_layout.tsx
@@ -1,2 +1,23 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#6CC51D",
+        tabBarInactiveTintColor: "#999",
+        tabBarStyle: {
+          height: 70,
+          paddingBottom: 12,
+          paddingTop: 8,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          backgroundColor: "#fff",
+          position: "absolute",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
