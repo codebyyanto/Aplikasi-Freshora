d261f6176909634b783ac8ed50f37dc65db4aaba
codebyyanto <yanto.unix@gmail.com>
2025-12-19 01:54:59 +0700

    inisialisasi halaman detail produk

---
+++ Freshora-mobile/app/product/[id].tsx
@@ -0,0 +1,45 @@
+// Import React dan hooks
+import React, { useEffect, useState } from "react";
+
+// Import komponen UI
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ScrollView,
+    ActivityIndicator,
+    Alert
+} from "react-native";
+
+// Import routing
+import { useLocalSearchParams, useRouter } from "expo-router";
+
+// Import icon
+import { Ionicons } from "@expo/vector-icons";
+
+// Import AsyncStorage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Import konfigurasi API
+import { API_BASE_URL, ENDPOINTS } from "../../constants/Config";
+
+// Helper format rupiah
+const formatCurrency = (value: number) => {
+    return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR"
+    }).format(value);
+};
+
+// Mapping gambar produk lokal
+const IMAGE_MAP: Record<string, any> = {
+    "peach.png": require("../../assets/images/products/peach.png"),
+    "avocado.png": require("../../assets/images/products/avocado.png"),
+    "pineapple.png": require("../../assets/images/products/pineapple.png"),
+    "grapes.png": require("../../assets/images/products/grapes.png"),
+    "pomegranate.png": require("../../assets/images/products/pomegranate.png"),
+    "broccoli.png": require("../../assets/images/products/broccoli.png"),
+    default: require("../../assets/images/products/peach.png")
+};
