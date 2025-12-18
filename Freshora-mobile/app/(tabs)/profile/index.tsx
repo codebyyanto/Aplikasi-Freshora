ff34c9875e36ba75f89887d888eabab43ae91168
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-12-19 03:56:58 +0700

    impor hook navigasi dan utilitas data

--- Freshora-mobile/app/(tabs)/profile/index.tsx
+++ Freshora-mobile/app/(tabs)/profile/index.tsx
@@ -1,2 +1,6 @@
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_BASE_URL, ENDPOINTS } from "../../../constants/Config";
+import React, { useEffect, useState } from "react";
