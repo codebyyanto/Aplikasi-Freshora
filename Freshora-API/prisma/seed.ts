c43bda5856167c0cd590d03a968903fc99655679
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-12-19 04:52:47 +0700

    definisi data awal kategori produk

--- Freshora-API/prisma/seed.ts
+++ Freshora-API/prisma/seed.ts
@@ -4,4 +4,11 @@ import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
-// 1. Setup Categories
+  // 1. Setup Categories
+  const categoriesData = [
+    { name: 'Vegetables' },
+    { name: 'Buah' },
+    { name: 'Minuman' },
+    { name: 'Kebutuhan' },
+    { name: 'Minyak nabati' }
+  ];
