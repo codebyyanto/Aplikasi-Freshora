bf7d7f2821e3724ca7dd515d5d808a4e454e4eb9
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-12-19 04:07:37 +0700

    update bable.config.js menabahkan navigasi Expo Router

--- Freshora-mobile/babel.config.js
+++ Freshora-mobile/babel.config.js
@@ -3,6 +3,10 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+
+      // untuk navigasi Expo Router
+      "expo-router/babel",
+
       // agar animasi react-native-reanimated berfungsi
       "react-native-reanimated/plugin",
     ],
