a3104338837cbb55e99b2ddd1ba4bc6cd89ad0de
sabrinazara <sabrina_az_zahra@teknokrat.ac.id>
2025-12-19 04:50:36 +0700

    menambahkan file seed.ts untuk di push ke database

--- Freshora-mobile/babel.config.js
+++ Freshora-mobile/babel.config.js
@@ -4,9 +4,6 @@ module.exports = function (api) {
     presets: ["babel-preset-expo"],
     plugins: [
 
-      // untuk navigasi Expo Router
-      "expo-router/babel",
-
       // agar animasi react-native-reanimated berfungsi
       "react-native-reanimated/plugin",
     ],
