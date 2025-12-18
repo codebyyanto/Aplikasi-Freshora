7ab65808cb8937fe4dd85a76a8cb016875c41f71
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-12-19 02:36:21 +0700

    impor core react dan komponen react-native

---
+++ Freshora-mobile/app/(tabs)/cart.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useCallback } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    FlatList,
+    Alert,
+    Dimensions,
+    Platform
+} from "react-native";
+
