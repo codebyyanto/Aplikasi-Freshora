f166fc9ca90aee4d4e0e3131babc7e07b6bfd87b
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-12-19 02:37:06 +0700

    impor gesture handler dan penyimpanan lokal

--- Freshora-mobile/app/(tabs)/cart.tsx
+++ Freshora-mobile/app/(tabs)/cart.tsx
@@ -12,3 +12,7 @@ import {
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter, useFocusEffect } from "expo-router";
+
+import { GestureHandlerRootView, Swipeable } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SafeAreaView } from "react-native-safe-area-context";
