c2684c9ddb32f77b02854c7ea9617e793f014eb1
codebyyanto <yanto.unix@gmail.com>
2025-12-18 07:39:51 +0700

    menambahkan komponen dan state detail produk

--- Freshora-mobile/app/(tabs)/home/[id].tsx
+++ Freshora-mobile/app/(tabs)/home/[id].tsx
@@ -17,3 +17,18 @@ import { Ionicons } from "@expo/vector-icons";
 
 // Import routing dan parameter URL dari Expo Router
 import { useLocalSearchParams, useRouter } from "expo-router";
+
+// Komponen halaman Detail Produk
+export default function ProductDetail() {
+
+    // Mengambil parameter id produk dari URL
+    const { id } = useLocalSearchParams();
+
+    // State untuk menyimpan data produk
+    const [product, setProduct] = useState<any>(null);
+
+    // State untuk menyimpan jumlah (quantity) produk
+    const [qty, setQty] = useState(1);
+
+    // Router untuk navigasi halaman
+    const router = useRouter();
