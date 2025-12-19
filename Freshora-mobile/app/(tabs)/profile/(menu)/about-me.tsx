789d308f92b7f040f1327687b99df4fa5e5c70a5
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-12-20 03:18:20 +0700

    impor navigasi expo-router dan hooks react

--- Freshora-mobile/app/(tabs)/profile/(menu)/about-me.tsx
+++ Freshora-mobile/app/(tabs)/profile/(menu)/about-me.tsx
@@ -1,1 +1,4 @@
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import { Stack, Tabs, useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
