cee855f47d177046fd6e22c8ebf01637310ac255
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2025-12-20 03:18:46 +0700

    impor async-storage dan konfigurasi API

--- Freshora-mobile/app/(tabs)/profile/(menu)/about-me.tsx
+++ Freshora-mobile/app/(tabs)/profile/(menu)/about-me.tsx
@@ -2,3 +2,6 @@ import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-nati
 import { Stack, Tabs, useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
 import React, { useEffect, useState } from "react";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { API_BASE_URL, ENDPOINTS } from "../../../../constants/Config";
