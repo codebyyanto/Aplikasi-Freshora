817329fb83640ca7636d6387abe2e022b181b526
fatimahlailatulazhra <fatimah_lailatul_azzahra@teknokrat.ac.id>
2026-01-16 21:57:52 +0700

    impor library react, react-native, dan dependensi navigasi

---
+++ Freshora-mobile/app/checkout/shipping.tsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter, useLocalSearchParams } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
